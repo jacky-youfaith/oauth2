07b8a4e69e823ba213f1666e6954cf40ad738a6f
jacky shang <shangwanxiang@hotmail.com>
2016-03-06 20:38:49 -0500

    First version of Youfaith.OAuth

---
+++ Youfaith.OAuth2/app.js
@@ -0,0 +1,62 @@
+﻿var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var oauthserver = require('oauth2-server');
+
+var app = express();
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//OAuth
+app.oauth = oauthserver({
+    model: {}, // See below for specification 
+    grants: ['password'],
+    debug: true
+});
+
+app.all('/oauth/token', app.oauth.grant());
+
+app.get('/', app.oauth.authorise(), function (req, res) {
+    res.send('Secret area');
+});
+
+app.use(app.oauth.errorHandler());
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+
+module.exports = app;
